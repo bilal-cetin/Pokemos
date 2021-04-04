326024f0ff4e5da2c6e59a7870becf37a5ece8f7
Bilal ÇETİN <bilal.cetin.4021@gmail.com>
2021-04-04 10:23:11 +0300

    Pokemon Seeder ve Factory Oluşturuldu

---
+++ database/factories/PokemonFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Pokemon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PokemonFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Pokemon::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name'=>$this->faker->name(),
+            'pokemon_photo_path'=>$this->faker->image(),
+            'age'=>$this->faker->numberBetween($min=1, $max=10),
+            'height'=>$this->faker->randomFloat($min=1, $max=10),
+            'evolves_from'=>$this->faker->text(),
+            'evolves_to'=>$this->faker->text(),
+            'weakness'=>$this->faker->text(),
+            'ability'=>$this->faker->text()
+        ];
+    }
+}
