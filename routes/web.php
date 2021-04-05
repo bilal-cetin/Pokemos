bfaf38c2c54ab276a3ea6e1ca7181961b08afb75
Bilal ÇETİN <bilal.cetin.4021@gmail.com>
2021-04-05 05:03:28 +0300

    Possessor ResourceController Oluşturuldu

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\PossessorController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,5 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::resource('possessors', PossessorController::class);
